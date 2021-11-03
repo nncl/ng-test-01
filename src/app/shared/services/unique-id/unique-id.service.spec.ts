f8be133a11cf559ef3806522503324c7ce758da4
Cauê Almeida <nncl@live.com>
2021-11-03 18:55:31 -0300

    test(services): Testing if prefix starts with `app-`

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -5,7 +5,7 @@ describe(UniqueIdService.name, () => {
   it(`#${UniqueIdService.prototype.generateIdWithPrefix.name} should generate an id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateIdWithPrefix('app');
-    expect(id).toContain('app-');
+    expect(id.startsWith('app-')).toBeTrue();
   });
 
 });
