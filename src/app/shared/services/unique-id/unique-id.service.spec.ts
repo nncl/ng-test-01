cf024347ab8e510ae1b0f0cf8187f26bf021f2a3
Cauê Almeida <nncl@live.com>
2021-11-03 18:13:10 -0300

    test(services): Refactoring tests' names

--- src/app/shared/services/unique-id/unique-id.service.spec.ts
+++ src/app/shared/services/unique-id/unique-id.service.spec.ts
@@ -1,8 +1,8 @@
 import { UniqueIdService } from './unique-id.service';
 
-describe('UniqueIdService', () => {
+describe(UniqueIdService.name, () => {
 
-  it('#generateIdWithPrefix should generate an id when called with prefix', () => {
+  it(`#${UniqueIdService.prototype.generateIdWithPrefix.name} should generate an id when called with prefix`, () => {
     const service = new UniqueIdService();
     const id = service.generateIdWithPrefix('app');
     expect(id).toContain('app-');
